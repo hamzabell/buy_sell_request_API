08c670a36b23a02988da1382c16049c04870daa5
Basseyak <Basseyak@ha-shem.com>
2021-10-05 11:09:19 +0100

    initial setup of express server

---
+++ index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.on("open", () => console.log("Connected to Database"));
+
+app.listen(PORT, () => {
+  console.info(`Server started at ${PORT}`);
+});
